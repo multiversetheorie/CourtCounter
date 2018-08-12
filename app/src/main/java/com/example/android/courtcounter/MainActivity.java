8e95c7ea7b210ecd7ff2a0ada0fa3526fa068a0b
Ádám Kovács <35578457+multiversetheorie@users.noreply.github.com>
2018-08-12 18:23:25 +0200

    Added button functionality methods.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.courtcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,4 +21,27 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * This method is called when the "+3 points" button is clicked.
+     */
+    public void threepointer(View view) {
+        int quantity = 3;
+        displayForTeamA(quantity);
+    }
+
+    /**
+     * This method is called when the "+2 points" button is clicked.
+     */
+    public void twopointer(View view) {
+        int quantity = 2;
+        displayForTeamA(quantity);
+    }
+
+    /**
+     * This method is called when the "+3 points" button is clicked.
+     */
+    public void freethrow(View view) {
+        int quantity = 1;
+        displayForTeamA(quantity);
+    }
 }
